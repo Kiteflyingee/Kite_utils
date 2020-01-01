37ecb2482c52f44ac44fbecfe659828ecbd6f590
devkite <bjliujin@126.com>
2020-01-01 13:34:39 +0800

    创建支持传参数的单例装饰器

--- single_instance.py
+++ single_instance.py
@@ -9,7 +9,7 @@ def singleton(cls, *args, **kwargs):
     """
     instance = {}
 
-    def _instance():
+    def _instance(*args, **kwargs):
         if cls not in instance:
             instance[cls] = cls(*args, *kwargs)
         return instance[cls]
